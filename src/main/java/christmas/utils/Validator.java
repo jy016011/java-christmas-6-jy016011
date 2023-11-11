64fe3bb412e92ceebc89bdd66b2c6695034b57a6
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-11-12 00:09:40 +0900

    style: rename method of Validator class and change Exception message
    Validator클래스의 함수 이름들을 유틸클래스의 용도에 맞도록
    어디에서든 사용할 수 있는 듯한 이름으로 변경하였으며,
    예외 발생시 출력하는 오류메시지도 특정한 상황이 아닌
    일반적인 상황에서 쓰일 수 있도록 문구 수정함.

--- src/main/java/christmas/utils/Validator.java
+++ src/main/java/christmas/utils/Validator.java
@@ -12,28 +12,35 @@ public class Validator {
     public static void validateIsNumber(String input) {
         boolean isNotNumber = !input.chars().allMatch(Character::isDigit);
         if (isNotNumber) {
-            raiseIllegalArgumentException(ERROR_MESSAGE_HEADER + " 숫자만 입력해주세요.");
+            raiseIllegalArgumentException(ERROR_MESSAGE_HEADER + " 숫자가 아닙니다.");
         }
     }
 
     public static void validateNotLessThan(int checkingNumber, int standardNumber) {
         if (checkingNumber < standardNumber) {
-            raiseIllegalArgumentException(ERROR_MESSAGE_HEADER + " " + standardNumber + " 이상의 수를 입력하세요.");
+            raiseIllegalArgumentException(ERROR_MESSAGE_HEADER + " " + standardNumber + " 이상의 수가 아닙니다.");
         }
     }
 
     public static void validateNotGreaterThan(int checkingNumber, int standardNumber) {
         if (checkingNumber > standardNumber) {
-            raiseIllegalArgumentException(ERROR_MESSAGE_HEADER + " " + standardNumber + " 이하의 수를 입력하세요.");
+            raiseIllegalArgumentException(ERROR_MESSAGE_HEADER + " " + standardNumber + " 이하의 수가 아닙니다.");
         }
     }
 
-    public static void validateUniqueNames(List<String> names) {
+    public static void validateIsEqual(int checkingNumber, int standardNumber) {
+        boolean notEqual = checkingNumber != standardNumber;
+        if (notEqual) {
+            raiseIllegalArgumentException(ERROR_MESSAGE_HEADER + " " + standardNumber + "와 같은 수가 아닙니다.");
+        }
+    }
+
+    public static void validateIsUnique(List<String> inputs) {
         boolean isDuplicated = (
-                names.stream().distinct().count() != names.size()
+                inputs.stream().distinct().count() != inputs.size()
         );
         if (isDuplicated) {
-            raiseIllegalArgumentException(ERROR_MESSAGE_HEADER + " 중복되지 않는 이름만 입력하세요.");
+            raiseIllegalArgumentException(ERROR_MESSAGE_HEADER + " 중복된 입력이 있습니다.");
         }
     }
 
