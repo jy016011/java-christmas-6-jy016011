8e992b3d80599e7daadf19aba2971e1f31aa7448
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-11-12 00:25:12 +0900

    refactor: combine duplicated logic in ResultServiceTest
    ResultServiceTest에서
    요구사항에서 예시로 제시한 12월 3일의 이상적인 실행예시와 같이
    입력하여 테스트하는 부분의 코드들의 반복되는 부분들을 한 함수로 합침

--- src/test/java/christmas/ResultServiceTest.java
+++ src/test/java/christmas/ResultServiceTest.java
@@ -39,53 +39,25 @@ public class ResultServiceTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("12월 3일에 다음과 같이 주문하면, 할인 전 총금액은 142,000원 일 것이다.")
+    @DisplayName("12월 3일에 실행 에시와 같이 주문하면, "
+            + "할인 전 총금액은 142,000원 일 것이고,"
+            + "샴페인 1개를 증정받아 25,000원을 혜택받을 것이며,"
+            + "총 할인 금액은 6,246원일 것이라,"
+            + "총 혜택 금액은 31,246원일 것이고,"
+            + "따라서 할인된 총금액은 135,754원 일 것이다.")
     @Test
     void getTotalPriceByThisOrder() {
         ResultService resultService = new ResultService();
         resultService.setDate("3");
         resultService.setOrder("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
         assertThat(resultService.getTotalPrice()).isEqualTo(142_000);
-    }
-
-    @DisplayName("12월 3일에 다음과 같이 주문하면, 샴페인을 증정받아 25,000원 이득을 볼 것이다.")
-    @Test
-    void getGiftBenefitByThisOrder() {
-        ResultService resultService = new ResultService();
-        resultService.setDate("3");
-        resultService.setOrder("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
         assertThat(resultService.getGiftBenefit()).isEqualTo(25_000);
-    }
-
-    @DisplayName("12월 3일에 다음과 같이 주문하면, 할인 금액이 6,246원일 것이다.")
-    @Test
-    void getTotalDiscountByThisOrder() {
-        ResultService resultService = new ResultService();
-        resultService.setDate("3");
-        resultService.setOrder("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
         assertThat(resultService.getTotalDiscount()).isEqualTo(6_246);
-    }
-
-    @DisplayName("12월 3일에 다음과 같이 주문하면, 총 혜택 금액이 31,246원일 것이다.")
-    @Test
-    void getTotalBenefitByThisOrder() {
-        ResultService resultService = new ResultService();
-        resultService.setDate("3");
-        resultService.setOrder("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
         assertThat(resultService.getTotalBenefit()).isEqualTo(31_246);
-    }
-
-
-    @DisplayName("12월 3일에 다음과 같이 주문하면, 할인된 총금액은 135,754원일 것이다.")
-    @Test
-    void getTotalByThisOrder() {
-        ResultService resultService = new ResultService();
-        resultService.setDate("3");
-        resultService.setOrder("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
         assertThat(resultService.getTotalDiscountedPrice()).isEqualTo(135_754);
     }
 
-    @DisplayName("만원이하로 주문하면, 어떤 이벤트도 받지 못한다.")
+    @DisplayName("만원 미만으로로 주문하면, 어떤 이벤트도 받지 못한다.")
     @Test
     void getNoEventsByUnderMinPrice() {
         ResultService resultService = new ResultService();
