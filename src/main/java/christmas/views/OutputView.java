611ddde2650a770dee7b658ef9ac123f322d1486
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-11-13 12:07:05 +0900

    feat: add feature of print greetings when application started
    프로그램 시작시 인삿말을 출력하는 기능을 추가하고, 검증함

--- src/main/java/christmas/views/OutputView.java
+++ src/main/java/christmas/views/OutputView.java
@@ -1,2 +1,12 @@
-package christmas.views;public class OutputView {
+package christmas.views;
+
+public class OutputView {
+    private static final int MONTH = 12;
+
+    private OutputView() {
+    }
+
+    public static void printGreetings() {
+        System.out.println("안녕하세요! 우테코 식당 " + MONTH + "월 이벤트 플래너입니다.");
+    }
 }
--- src/test/java/christmas/views/OutputViewTest.java
+++ src/test/java/christmas/views/OutputViewTest.java
@@ -1,2 +1,41 @@
-package christmas.views;public class OutputViewTest {
+package christmas.views;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OutputViewTest {
+    private static final int MONTH = 12;
+
+    private PrintStream standardOut;
+    private ByteArrayOutputStream captor;
+
+    @BeforeEach
+    public void init() {
+        standardOut = System.out;
+        captor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(captor));
+    }
+
+    @AfterEach
+    public void printOutput() {
+        System.setOut(standardOut);
+        captor.reset();
+    }
+
+    private String output() {
+        return captor.toString().trim();
+    }
+
+    @DisplayName("플래너 인삿말 출력잘하는지 확인")
+    @Test
+    void testPrintingGreetings() {
+        OutputView.printGreetings();
+        assertThat(output()).isEqualTo("안녕하세요! 우테코 식당 " + MONTH + "월 이벤트 플래너입니다.");
+    }
 }
