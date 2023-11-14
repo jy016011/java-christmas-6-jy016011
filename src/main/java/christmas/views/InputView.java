a5876b80b5830348cfd068788a7c873c3cbd9c8e
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-11-14 14:34:45 +0900

    add: change output message in InputView class
    "주문하실 메뉴를 메뉴와 개수를 알려 주세요."
    에서 다시
    "주문하실 메뉴와 개수를 알려 주세요."로 수정함.
    실행예시에는 첫 번쨰 문장처럼 어색한 어휘로 되어있지만,
    개발팀의 요구사항에는 두번쨰 문장같이 맞는 어휘로 요구되어있어
    다시 수정함

--- src/main/java/christmas/views/InputView.java
+++ src/main/java/christmas/views/InputView.java
@@ -14,7 +14,7 @@ public class InputView {
     }
 
     public static String getOrderInput() {
-        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        System.out.println("주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         return Console.readLine();
     }
 }
